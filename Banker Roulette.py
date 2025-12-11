d2959b8cd373e88da9b1e587afe951b8ec5da2f5
Sead Abduli <abdulisead@icloud.com>
2025-12-11 12:14:22 +0100

    Update Banker Roulette.py

--- Banker Roulette.py
+++ Banker Roulette.py
@@ -3,3 +3,9 @@ import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 
 print(random.choice(friends))
+
+
+# option 2
+
+random_index = random.randit(0, 4)
+print(friends[random.index])
